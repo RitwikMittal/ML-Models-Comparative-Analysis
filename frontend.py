919b45abcea3ad108e957fe3813fba929fad37e9
Ritwik Mittal <mittalritwik50@outlook.com>
2025-05-12 12:32:35 +0530

    FrontEnd Dashboard Final

--- frontend.py
+++ frontend.py
@@ -465,3 +465,6 @@ def main():
     # Footer
     st.markdown('<div class="footer">Made by Ritwik Mittal & Yashas Raina<b><b></div>', 
                 unsafe_allow_html=True)
+    
+if __name__ == "__main__":
+    main()
